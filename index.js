55fef96030023e0a282885b957318b47e770cb36
Danko Karleusa <dankomatichevich@gmail.com>
2022-03-15 11:36:02 -0700

    commit tree 4724522db721cda3d5260b543c33926d2160f165

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.ceil(Math.random());
+
+let num3 = 14
+let num4 = 10
+const mod = num3 - num4
+
+const numbers = [5, 10, 15, 20]
+
+const max = Math.max(1, 5, 20)
